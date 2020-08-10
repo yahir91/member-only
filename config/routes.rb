ce4a86e5d3b2fe8d51f742437efd9c9c7b393022
Yahir Cardona <yahir.cardona@gmail.com>
2020-08-10 15:08:39 -0600

    controllers for member created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  devise_for :members, controllers: { sessions: 'members/sessions', registrations: 'members/registrations' }
+  get '/members/registrations/new', to: "members/registrations#new"
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
