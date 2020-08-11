9ac919d9cc1db06660a1d4faa5316890b887473c
maosan132 <maosan132@gmail.com>
2020-08-11 10:11:49 -0500

    added authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_member!, only: %i[new, create]
+  before_action :authenticate_member!, only: %i[new, create]
   before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_member!, except: %i[index]
 
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1
   # GET /posts/1.json
-  #def show; end
+ def show; end
 
   # GET /posts/new
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :members, controllers: { sessions: 'members/sessions', registrations: 'members/registrations' }
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show]
   resources :members
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
